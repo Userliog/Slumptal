aa67f2e06876169c129092c99f1c17ae256c94eb
Userliog <92377001+Userliog@users.noreply.github.com>
2021-10-12 16:06:58 +0200

    Add files via upload

---
+++ Gissa slumptal 1 till 100/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Gissa_slumptal_1_till_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random slump = new Random();
+            int random = slump.Next(1,101);
+
+            int AntalGissningar = 0;
+
+            Console.Write("Gissa det slumpade talet: ");
+            
+            while (true)
+            {
+            string str1 = Console.ReadLine();
+            int Gissning = int.Parse(str1);
+                
+                AntalGissningar++;
+
+                if (Gissning < random)
+                    {
+                        Console.Write("Gissningen är för låg. Försök igen: ");
+                    } 
+                else
+                    if (Gissning > random)
+                        {
+                            Console.Write("Gissningen är för hög. Försök igen: ");
+                        }
+                    else
+                        {
+                            Console.WriteLine("Grattis! "+random+" är rätt tal, det tog "+AntalGissningar+" antal försök.");
+                        }
+                }
+            }
+        }
+    }
+
